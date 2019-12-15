999d1b91a26fa155070a216e9fe8b3246f145adf
claytantor <claytantor@gmail.com>
2019-12-15 09:05:39 -0800

    trying to get tags to work

--- iac/bash/version.sh
+++ iac/bash/version.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-GIT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
-VERSION=1.5
-if [ $GIT_BRANCH == "master" ]
-then
-    VERSION=1.3
-fi
-echo $VERSION
+echo $(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
 
